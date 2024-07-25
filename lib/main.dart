9c22d8fa516a6b6459a692238cf006531697a619
Shamim-Raihan <shamimraihan91@gmail.com>
2024-07-25 22:42:03 +0600

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:indrive/screens/auth_screen/register_screen.dart';
+import 'package:indrive/screens/auth_screen/views/register_screen.dart';
 
 void main() {
   runApp(const MyApp());
